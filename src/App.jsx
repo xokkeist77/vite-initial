28a68a239cf4454ba2e1c1df99de3a0003ff6a9a
Dmitry Kiryak <dkiryak@gmail.com>
2024-12-11 16:54:11 +0300

    refactor without JSX code

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,79 @@
-import ReactLogo from './assets/react.svg?react';
+import reactLogo from './assets/react.svg';
+import { createElement } from 'react';
+
 //import './index.css';
 
-export const App = () => {
-	const date = new Date().getFullYear(); // декларативный стиль
-	return (
-		<div className="App">
-			<header className="App-header">
-				{/* декларативный стиль от этой точки */}
-				<ReactLogo />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{date} год</p>
-				{/* декларативный стиль до этой точки */}
-			</header>
-		</div>
+// export const App = () => {
+// 	const date = new Date().getFullYear(); // декларативный стиль
+// 	return (
+// 		<div className="App">
+// 			<header className="App-header">
+// 				{/* декларативный стиль от этой точки */}
+// 				<ReactLogo />
+// 				<p>
+// 					Edit <code>src/App.js</code> and save to reload.
+// 				</p>
+// 				<a
+// 					className="App-link"
+// 					href="https://reactjs.org"
+// 					target="_blank"
+// 					rel="noopener noreferrer"
+// 				>
+// 					Learn React
+// 				</a>
+// 				<p>{date} год</p>
+// 				{/* декларативный стиль до этой точки */}
+// 			</header>
+// 		</div>
+// 	);
+// };
+
+const date = `${new Date().getFullYear()} год`;
+
+const DivApp = () => {
+	return createElement('div', { className: 'App' }, Header());
+};
+
+const Header = () => {
+	return createElement(
+		'header',
+		{ className: 'App-header' },
+		ImageBox(),
+		TextParagraph1(),
+		RefLink(),
+		DateParagraph(),
 	);
+};
+
+const ImageBox = () => {
+	return createElement('img', { className: 'App - logo', src: reactLogo });
+};
+
+const TextParagraph1 = () => {
+	return createElement('p', null, `Edit `, CodeText(), ` and save to reload.`);
+};
+
+const DateParagraph = () => {
+	return createElement('p', null, date);
+};
+
+const CodeText = () => {
+	return createElement('code', null, 'src/App.js');
+};
+
+const RefLink = () => {
+	return createElement(
+		'a',
+		{
+			className: 'App-link',
+			href: 'https://reactjs.org',
+			target: '_blank',
+			rel: 'noopener noreferrer',
+		},
+		'Learn React',
+	);
+};
+
+export const App = () => {
+	return DivApp();
 };
